5c880b3648d7443bbaba0e556023f6ea0f88c80d
JokinAU <jokinau@gmail.com>
2016-11-23 18:22:34 +0100

    Committing from PowerUbu to GitHub

---
+++ USB/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<title>GPG keys</title>
+	</head>
+	<body>
+		<p><a href="private-key.gpg">private-key.gpg</a></p>
+		<p><a href="public-key.gpg">public-key.gpg</a></p>
+		<p><a href="secret-text.gpg">secret-text.gpg</a></p>
+	</body>
+</html>
