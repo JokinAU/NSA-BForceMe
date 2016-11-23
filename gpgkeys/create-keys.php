7ba0d89b26cfb447b946c2c6d31313829fa87aa5
JokinAU <jokinau@gmail.com>
2016-11-23 15:02:47 +0100

    Committing from PowerUbu to GitHub

--- gpgkeys/create-keys.php
+++ gpgkeys/create-keys.php
@@ -1,9 +1,13 @@
 <?php
-$templatein = './create-keys.answers-template';
-$templateout = './create-keys.answers';
+$templatein =      './create-keys.answers-template';
+$templateout =     './create-keys.answers';
 
-$secretfile = 'secret-text.txt';
-$certid = 'Secret Password (NSA\'s best agents will never be known) <info@develdevil.net>';
+$secretfileplain = './secret-text.txt';
+$secretfile =      '../USB/secret-text.gpg';
+$publicfile =      '../USB/public-key.gpg';
+$privatefile =     '../USB/private-key.gpg';
+
+$certid = 'Secret Password (best secret agents will never be known) <info@develdevil.net>';
 
 //PREPARE TEMPLATE
 //Read the template file
@@ -21,21 +25,21 @@ $keygeneration = shell_exec('gpg --batch --gen-key create-keys.answers');
 //echo 'Key generation: '.$keygeneration."\n";
 
 //Export public key to readable format
-$output = shell_exec('gpg -a --export "'.$certid.'" > public-key.gpg');
+$output = shell_exec("gpg -a --export '$certid' > $publicfile");
 //Export secret key to readable format
-$output = shell_exec('gpg -a --export-secret-keys "'.$certid.'" > private-key.gpg');
+$output = shell_exec("gpg -a --export-secret-keys '$certid' > $privatefile");
 
 //Create secret file contents
 require_once('../include/functions.php');
-file_put_contents($secretfile, NSA_lipsum(32,'bytes',0));
+file_put_contents($secretfileplain, NSA_lipsum(32,'bytes',0));
 //Encrypt file
-$encryption = shell_exec('gpg --batch --yes -ar "'.$certid.'" --output secret-text.gpg --encrypt secret-text.txt');
+$encryption = shell_exec("gpg --batch --yes -ar '$certid' --output $secretfile --encrypt $secretfileplain");
 
 //DELETE PLAIN TEXT
-unlink($secretfile);
+unlink($secretfileplain);
 //DELETE KEY FROM SYSTEM:
 //Run command to get the key's fingerprint
-$rawfingerprint = shell_exec('gpg --list-keys --fingerprint "'.$certid.'"');
+$rawfingerprint = shell_exec("gpg --list-keys --fingerprint '$certid'");
 
 //split its lines
 $fingerprintlines = explode("\n", $rawfingerprint);
@@ -51,8 +55,8 @@ foreach ($lines as $line):
 	$fingerprint = $parts[1];
 
 	//Delete secret key
-	shell_exec('gpg --batch --yes --delete-secret-keys "'.$fingerprint.'"');
+	shell_exec("gpg --batch --yes --delete-secret-keys '$fingerprint'");
 	//Delete key
-	shell_exec('gpg --batch --yes --delete-keys "'.$fingerprint.'"');
+	echo shell_exec("gpg --batch --yes --delete-keys '$fingerprint'");
 endforeach;
 ?>
